6a2c32619e9e8986aec22b7d6938ce1245c11383
harveybellini <harveybellini@gmail.com>
2023-02-08 15:09:32 +0000

    all

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-    if (details.url.indexOf("amazon.com") != -1) {
-      chrome.tabs.update(details.tabId, {
-        url: details.url.replace("amazon.com", "amazon.co.uk")
-      });
-    }
-  });
-  
+chrome.tabs.onUpdated.addListener(function(activeInfo) {
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+  // since only one tab should be active and in the current window at once
+  // the return variable should only have one entry
+  var activeTab = tabs[0];
+  console.log(activeTab.url);
+  alert(activeTab.url);
+});
+});
